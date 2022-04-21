6d98d79f30696774e4cb308f1cf08495479f98a6
Douglas dos Santos Oliveira <doug1306@gmail.com>
2022-04-20 21:52:40 -0300

    arrumando bug

--- covid19-dio/src/componets/WorldValue/index.tsx
+++ covid19-dio/src/componets/WorldValue/index.tsx
@@ -8,15 +8,15 @@ export const WorldsValues = ({world}: Props) => {
     <>
       <div className="Cases">
         <h3>Casos</h3>
-        <h1>{world.cases.toLocaleString("en-US")}</h1>
+        <h1>{world?.cases.toLocaleString("en-US")}</h1>
       </div>
       <div className="Deaths">
         <h3>Mortes</h3>
-        <h1>{world.deaths.toLocaleString("en-US")}</h1>
+        <h1>{world?.deaths.toLocaleString("en-US")}</h1>
       </div>
       <div className="Recovered">
         <h3>Recuperados</h3>
-        <h1>{world.recovered.toLocaleString("en-US")}</h1>
+        <h1>{world?.recovered.toLocaleString("en-US")}</h1>
       </div>
     </>
   );
